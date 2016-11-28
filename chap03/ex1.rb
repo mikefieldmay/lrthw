c74c77986623ddeecfa701e68116dd3a360ad80f
Mike Field-May <mfieldmay@gmail.com>
2016-11-28 21:24:04 +0000

    Exercise 3

---
+++ chap03/ex1.rb
@@ -0,0 +1,27 @@
+# Will output the string below
+puts "I will now count my chickens:"
+# puts Hens 25 + (30/6)
+puts "Hens #{25 + 30 / 6}"
+# Puts Roosters 100 - (25 * 3 % 4)
+puts "Roosters #{100 - 25 * 3 % 4}"
+# Just put out the statement
+puts "Now I will count my eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 -1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+# Will print False
+puts 3 + 2 < 5 - 7
+
+puts "What is 3 + 2? #{3 + 2}"
+puts "What is 5 - 7? #{5 - 7}"
+
+puts "Oh, thats why it's false."
+
+puts "How about some more."
+#Will print true
+puts "Is it greater? #{5 > 2}"
+#will print true
+puts "Is it greater or equal? #{5 >= -2}"
+#will print false
+puts "Is it less or equal? #{5 <= -2}"
---
+++ chap03/payCalculator.rb
@@ -0,0 +1,3 @@
+weekly = 37.5 * 7.25
+puts "My weekly wage is #{weekly}"
+puts "My monthly earnings are #{weekly * 52 / 12}"
