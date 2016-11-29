68b8dbce4710f39fc5643001c85bc579fca4afd3
Mike Field-May <mfieldmay@gmail.com>
2016-11-29 20:10:07 +0000

    End of Chapter 7

---
+++ chap07/ex1.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamd." #outputs the line
+puts "It's fleece was white as #{'snow'}." #outputs the line with the string snow interpolated into the string
+puts "And everywhere that Mary went." #puts the line
+puts"." * 10 # what'd that do> puts 10 * .
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "b"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch what print vs. puts on this line what's it do? - print doesn't add a line afterwards whereas puts does
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
