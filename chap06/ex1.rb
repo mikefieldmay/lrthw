5c43b0697a3f1f1d884b2cb4f35928dd6d2b661a
Mike Field-May <mfieldmay@gmail.com>
2016-11-29 20:01:29 +0000

    End of Chapter 6

---
+++ chap06/ex1.rb
@@ -0,0 +1,26 @@
+# sets the value of types of people to 10
+types_of_people = 10
+# creates a string variable that interpolates another variable
+x = "There are #{types_of_people} types of people."
+# creates new string variables
+binary = "binary"
+do_not = "don't"
+# Interpolates those string variables into another string variable
+y = "Those who know #{binary} and those who #{do_not}."
+# Outputs x and y, the two string variables that interpolated other variables.
+puts x
+puts y
+# Outputs the x and y, but using string interpolation
+puts "I said: #{x}."
+puts "I also said: #{y}."
+# Creates a variable with the value of false
+hilarious = false
+# Interpolates the false value into a new string variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#puts the new string variable
+puts joke_evaluation
+# Creates ne w string variables
+w = "This is the left side of..."
+e = "a string with a right side."
+#puts the combination of two strings added together
+puts w + e
