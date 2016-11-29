67498af3b38df73aee30b3f0c879dd10def0e778
Mike Field-May <mfieldmay@gmail.com>
2016-11-29 21:11:09 +0000

    End of Chapter 11

---
+++ chap11/ex2.rb
@@ -0,0 +1,9 @@
+puts "Hi there"
+puts "What's your name?"
+name = gets.chomp
+puts "Where are you from?"
+place = gets.chomp
+puts "What's your favourite food?"
+food = gets.chomp
+
+puts "Hello again #{name} from #{place}. I hope your'e hungry, I've got some #{food} for you."
