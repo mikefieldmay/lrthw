12ebb698dde7927a22cf899df166d830fb35559f
Mike Field-May <mfieldmay@gmail.com>
2016-11-30 18:50:16 +0000

    End of Chapter 19

---
+++ chap19/ex2.rb
@@ -0,0 +1,6 @@
+def cat_dance_party(number_of_cats, dance_type, location)
+  puts "There are #{number_of_cats} cats going to #{location} to do #{dance_type} dancing!"
+end
+
+cat_dance_party(12, "Tango", "Solva")
+cat_dance_party(2, "Salsa", "Haverfordwest")
