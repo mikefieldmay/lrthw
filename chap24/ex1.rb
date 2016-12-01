6a0835722734d45f8e3a811eeb9a4c8b90078f22
Mike Field-May <mfieldmay@gmail.com>
2016-12-01 21:57:00 +0000

    End of chapter 24

---
+++ chap24/ex1.rb
@@ -0,0 +1,38 @@
+puts "Lets preactice everything."
+puts 'You\'d need to know \'bout escapes with \\ that do \n and \t tabs.'
+
+# The end is a "heredoc". See the student questinos.
+poem = <<END
+\tThe lovely world
+with logic so firmly planted
+cannot discern \n the needs of love
+nor comprehend passion from intuition
+and requires an explanation
+\n\t\twhere there is none.
+END
+
+puts "------------"
+puts poem
+puts "------------"
+
+five = 10 -2 + 3 - 6
+puts "This should be five: #{five}"
+
+
+def secret_formula(started)
+  jelly_beans = started * 500
+  jars = jelly_beans / 1000
+  crates = jars / 100
+  return jelly_beans, jars, crates
+end
+
+start_point = 10000
+#because 3 variables are returned, we call the function with 3 variables
+beans, jars, crates = secret_formula(start_point)
+
+puts "With a starting point of: #{start_point}"
+puts "We'd have #{beans} beans, #{jars} jars, and #{crates} crates."
+
+start_point = start_point / 10
+puts "We can also do that this way:"
+puts "We'd have %s beans, %s jars, and %s crates." % secret_formula(start_point)
